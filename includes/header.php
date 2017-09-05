0eb97d7486e2642ada6803b7461d6251687ad8a7
Michal Nalevanko <michal.nalevanko@gmail.com>
2017-09-05 05:49:40 +0200

    Changed the search button, deleted necessary links

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,7 @@
 		<title>IBD Charts</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		
 	</head>
 	<body>
 		<nav class="navbar navbar-default">
@@ -14,15 +15,13 @@
 				</div>
 				
 				<ul class="nav navbar-nav">
-					<li><a href="index.php">Home</a></li>
-					<li><a href="edit_watchlist.php">Edit watchlist</a></li>
-					<li><a href="charts.php">Charts</a></li>				
+					<li><a href="edit_watchlist.php">Edit watchlist</a></li>				
 					
 					<form class="navbar-form navbar-left" method="POST" action="charts.php?symbol=single">
 						<div class="form-group">
 							<input type="text" class="form-control" name="tickers">						
 						</div>
-							<button type="submit" class="btn btn-deafult">Quick chart</button>
+							<button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
 						
 					</form>	
 				</ul>
